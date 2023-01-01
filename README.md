1f5bd1fbbd792aa1ffc90a085e16e9ac65a9e19f
John Basrai <John.Basrai@vts-i.com>
2022-12-31 17:01:15 -0800

    Fix broken symbolic link, update Readme with info ot install boost

--- README.md
+++ README.md
@@ -3,6 +3,23 @@ Using Boost Asio with modern C++. A brief high level introduction to boost asio.
 
 These examples where tested with boost 1.75
 
-https://www.geohub.com/installing-boost-on-rhel8/
+One way to install this version of asio is
 
+```
+mkdir -p ~/src
+cd ~/src
+wget https://boostorg.jfrog.io/artifactory/main/release/1.75.0/source/boost_1_75_0.tar.gz
+tar xf boost_1_75_0.tar.gz
+cd boost_1_75_0
+./bootstrap.sh \
+  --libdir=/usr/lib64 \
+  --with-libraries=system,thread,test,chrono,regex,date_time,filesystem,locale,random,atomic,log,program_options,exception
+./b2 -j 8
+sudo ./b2 install
+```
 
+You can search for newer version here
+
+https://boostorg.jfrog.io/artifactory/main/release/
+
+But you mileage may vary.
--- daytime-tcp/Refcountable.hh
+++ daytime-tcp/Refcountable.hh
@@ -1,1 +1,80 @@
-../../c++11/Refcountable.hh
+#ifndef Refcountable_hh
+#define Refcountable_hh
+
+#if __cplusplus < 201103L
+# error "Older versions of C++ not supported."
+#endif
+
+#include <atomic>
+
+namespace util 
+{
+    using atomic_type = std::atomic_int64_t;
+    static inline int64_t AtomicAdd(volatile atomic_type* counter, const int32_t toAdd)
+    {
+        const auto oldValue = counter->fetch_add(toAdd);
+
+        // Returns new value (after adding value).
+        return oldValue + toAdd;
+    }
+}
+
+#include <boost/assert.hpp>
+
+namespace util
+{
+    class Refcountable
+    {
+    protected:
+        virtual ~Refcountable(){};
+
+        // Optional delete notify, can be overridden by derived class to receive
+        // notification of a pending delete.
+        virtual void deleteNotify() {}
+
+    public:
+        // Public method called by intrusive_ptr<> template
+        void _addRef_()
+        {
+            BOOST_ASSERT(theRefcount >= 0);
+            (void)AtomicAdd(&theRefcount, 1);
+        }
+
+        // Public method to decrement reference count, called by intrusive_ptr<> template
+        // class via macro intrusive_ptr_release() in this file.
+        void _decRef_()
+        {
+            BOOST_ASSERT(theRefcount >= 0);
+            if ( AtomicAdd(&theRefcount,  -1) == 0 )
+            {
+                deleteNotify();
+                theRefcount = -1;
+                delete this;
+            }
+        }
+
+        // Returns current reference count for this object.
+        int refcount() const { return theRefcount; }
+
+    protected:
+        // Protected constructor initializes theRefcount to 0.
+        Refcountable() : theRefcount(0) {}
+
+    private:
+        atomic_type theRefcount;
+
+        Refcountable& operator=(const Refcountable&rhs);
+        Refcountable(Refcountable&);
+    };
+}
+
+#ifdef _inc_intrusive_ptr_add_ref_macro
+
+// Scaffolding required for boost/intrusive_ptr.hpp
+// Should be a template in a namespace but these macros can be used in a pinch
+//
+# define intrusive_ptr_add_ref(_ptr) do { _ptr->_addRef_(); } while(false)
+# define intrusive_ptr_release(_ptr) do { _ptr->_decRef_(); } while(false)
+#endif /* _inc_intrusive_ptr_add_ref_macro */
+
+#endif  // Refcountable_hh
